1776c110331ddbc28d6476114567845c0793705b
Ian Richard <ianrichard03@gmail.com>
2019-06-29 18:00:17 -0500

    added page to server.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //Dependencies
 const mongoose = require('mongoose');
 const express = require('express');
+const path = require('path');
 require('dotenv').config();
 
 //Database
@@ -17,6 +18,14 @@ playerOne.save().then( () => console.log('Added Player'));
 
 //Server
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+app.use(express.static(path.join(__dirname,"./public")));
 
 app.get('/', ( req, res) => res.send('I\'m listening.'));
 app.listen(process.env.PORT, () => console.log(`Server listening on port ${process.env.PORT}`));
